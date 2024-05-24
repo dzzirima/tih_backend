f0303a077e4a2475218c3d55a321ced8cc490b4f
dzzirima <dzzirima@gmail.com>
2024-05-24 10:57:18 +0200

    feat: addeding endpoint

--- src/main/java/blackmhofu/com/whatsapp/service/WhatsAppServiceImpl.java
+++ src/main/java/blackmhofu/com/whatsapp/service/WhatsAppServiceImpl.java
@@ -10,6 +10,9 @@ import com.whatsapp.api.impl.WhatsappBusinessCloudApi;
 import com.whatsapp.api.utils.Formatter;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 
 @Service
 public class WhatsAppServiceImpl {
@@ -24,21 +27,29 @@ public class WhatsAppServiceImpl {
 
         try {
 
-            System.out.println("doing whatsapp ");
-            WhatsappApiConfig.setApiVersion(ApiVersion.V18_0);
-            WhatsappApiFactory factory = WhatsappApiFactory.newInstance("EAAPTrljvzD8BO3yqZArZBEBBvXEF8suvTUQzhNRjF64frOuOAYcebU5WZA0uhHSheFZBeY4F4tunG58tCrtIXRGH59lSxPi6FzIPaE4tzZBux4aQPT9vcilw3BUZBdq1ejLiQSK7sySOBbONo4nzBAVBNrKrlL63m67dJHaZBsRylFRnVyQSdLxApbboB6XT0k3x9qnjtZAy1y9r6556fqIZD");
+//            System.out.println("doing whatsapp ");
+//            WhatsappApiConfig.setApiVersion(ApiVersion.V19_0);
+//            WhatsappApiFactory factory = WhatsappApiFactory.newInstance("EAAPTrljvzD8BO4xhg81Je2PXtUumQkDvjWJbedLIVuKO4OY4qK69QhdUkM7IcmB68g5XGzDn9ZCOGwTu3DVl5Kkz0VpOrcnJZBHn4VtbFGRdPBaG9Xcq7tZAvBwz6fv9ZA0Fg5nuxDVAhLc2aydsRfo8kZCu1hFPbRBdihlG7aYOCpGp8K17uV5jjtizZBdZAJe3Jn5toWWzhYigLtBZByMZD");
+//
+//            WhatsappBusinessCloudApi whatsappBusinessCloudApi = factory.newBusinessCloudApi();
+//            var message = Message.MessageBuilder.builder()//
+//                    .setTo("263785395827")//
+//                    .buildTextMessage(new TextMessage()//
+//                            .setBody(Formatter.bold("Hello world!") + "\nSome code here: \n" + Formatter.code("hello world code here"))//
+//                            .setPreviewUrl(false));
+//
+//
+//            MessageResponse messageResponse = whatsappBusinessCloudApi.sendMessage("315089391668255", message);
+//
+//            System.out.println(messageResponse);
 
-            WhatsappBusinessCloudApi whatsappBusinessCloudApi = factory.newBusinessCloudApi();
-            var message = Message.MessageBuilder.builder()//
-                    .setTo("263785395827")//
-                    .buildTextMessage(new TextMessage()//
-                            .setBody(Formatter.bold("Hello world!") + "\nSome code here: \n" + Formatter.code("hello world code here"))//
-                            .setPreviewUrl(false));
 
+            // Doing it manually
 
-            MessageResponse messageResponse = whatsappBusinessCloudApi.sendMessage("315089391668255", message);
-
-            System.out.println(messageResponse);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI("https://graph.facebook.com/v19.0/315089391668255/messages"))
+                    .GET()
+                    .build();
 
 
         }catch (Exception e){
